10c0cce5066c15dca7389e37ecda38816e68863e
Suraj Prakash <surajsingh7k@gmail.com>
2025-02-10 21:36:58 +0530

    Add files via upload

---
+++ Day_7.py
@@ -0,0 +1,8 @@
+### Dimond star Pattern program    complete      =====================================================================
+
+n=int(input("Enter the row number :"))
+for i in range (n):
+    print(" "*(n-i-1)+"*"*(2*i+1))
+    
+for i in range(n-2,-1,-1):
+    print(" " * (n-i-1)+"*"*(2*i+1))
